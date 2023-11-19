09cb7852caf5504c0acf2badf64bafb07f199d38
blake-butsko <89292796+blake-butsko@users.noreply.github.com>
2023-11-19 17:04:46 -0500

    late inital commit

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+  import { createClient } from '@supabase/supabase-js'
+
+  export const supabase = createClient('https://gweyysimvbqzxyrppzbs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3ZXl5c2ltdmJxenh5cnBwemJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI3NTc1NzYsImV4cCI6MjAwODMzMzU3Nn0.5HLAuwVl5ugo2a3ykH-OoaOXGTeZ_nwFeLFDSEYZ0dk')
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let data;
+  </script>
+
+<head>	  
+	  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	  <title>Page</title>
+</head>
+
+  <ul>
+    {#each data.countries as country}
+      <li>{country.name}</li>
+    {/each}
+  </ul>
